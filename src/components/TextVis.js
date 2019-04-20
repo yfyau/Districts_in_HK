f7f641bf9b25a72f6cf401d9698f86d06dc8959c
donfour <donovanso@yahoo.com.hk>
2019-04-20 21:39:24 +0800

    textcloud now has dropdown menu to let users choose from 18 districts

--- src/components/TextVis.js
+++ src/components/TextVis.js
@@ -1,22 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import randomColor from 'randomcolor';
 import TagCloud from 'react-tag-cloud';
 import CloudItem from './CloudItem';
 import '../styles/TextVis.css';
 import * as d3 from "d3";
 
-const json = require('../data/google-trends/中西區/中西區.json');
+const renderWordCloud = (district) => {
 
-const valueArr = json.map(el => el.value);
+  const json = require(`../data/google-trends/${district}/${district}.json`);
 
-var fontSizeScale = d3.scaleLinear()
-  .domain([Math.min(...valueArr), Math.max(...valueArr)])
-  .range([20, 50]);
+  const valueArr = json.map(el => el.value);
 
-const TextVis = () => (
+  const fontSizeScale = d3.scaleLinear()
+    .domain([Math.min(...valueArr), Math.max(...valueArr)])
+    .range([20, 50]);
+
+  return (
+    json.map(({query, value}, i) => (
+        <CloudItem
+            key={i}
+            text={query}
+            value={value}
+            style={{fontSize: fontSizeScale(value)}}
+        />
+    ))
+  );
+}
+
+const TextVis = () => {
+  
+  const [district, setDistrict] = useState('中西區');
+
+  return (
     <div className='app-outer'>
         <div className='app-inner'>
-          <h1>中西區</h1>
+        <select name="districts" onChange={event => setDistrict(event.target.value)}>
+          <option value="中西區">中西區</option>
+          <option value="九龍城區">九龍城區</option>
+          <option value="元朗區">元朗區</option>
+          <option value="北區">北區</option>
+          <option value="南區">南區</option>
+          <option value="大埔區">大埔區</option>
+          <option value="屯門區">屯門區</option>
+          <option value="東區">東區</option>
+          <option value="沙田區">沙田區</option>
+          <option value="油尖旺區">油尖旺區</option>
+          <option value="深水埗區">深水埗區</option>
+          <option value="灣仔區">灣仔區</option>
+          <option value="荃灣區">荃灣區</option>
+          <option value="葵青區">葵青區</option>
+          <option value="西貢區">西貢區</option>
+          <option value="觀塘區">觀塘區</option>
+          <option value="離島區">離島區</option>
+          <option value="黃大仙區">黃大仙區</option>
+        </select>
           <TagCloud 
             className='tag-cloud'
             style={{
@@ -25,16 +62,10 @@ const TextVis = () => (
               color: () => randomColor({ hue: 'blue' }),
               padding: 5,
             }}>
-            {json.map(({query, value}) => (
-                <CloudItem
-                    text={query}
-                    value={value}
-                    style={{fontSize: fontSizeScale(value)}}
-                />
-            ))}
+            {renderWordCloud(district)}
           </TagCloud>
         </div>
     </div>
-);
+)};
 
 export default TextVis;
