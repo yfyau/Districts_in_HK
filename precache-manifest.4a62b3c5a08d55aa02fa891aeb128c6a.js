457ad840822197b64c19c43b73c944060252b5ce
donfour <donovanso@yahoo.com.hk>
2019-05-02 21:56:54 +0800

    Updates

---
+++ precache-manifest.4a62b3c5a08d55aa02fa891aeb128c6a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66605fee74916f7fca51d986daeff8d",
+    "url": "/Districts_in_HK/index.html"
+  },
+  {
+    "revision": "77004c124265905056d5",
+    "url": "/Districts_in_HK/static/css/2.172d1813.chunk.css"
+  },
+  {
+    "revision": "e0fd24a7cf39bfda2f28",
+    "url": "/Districts_in_HK/static/css/main.6d98d68a.chunk.css"
+  },
+  {
+    "revision": "77004c124265905056d5",
+    "url": "/Districts_in_HK/static/js/2.2dcf525d.chunk.js"
+  },
+  {
+    "revision": "e0fd24a7cf39bfda2f28",
+    "url": "/Districts_in_HK/static/js/main.bb8f6b4c.chunk.js"
+  },
+  {
+    "revision": "c83797f3fccea26c83cc",
+    "url": "/Districts_in_HK/static/js/runtime~main.b6f75f80.js"
+  },
+  {
+    "revision": "1672892eb6d3e13b7cb3597df2f3b9fd",
+    "url": "/Districts_in_HK/static/media/0100_0500_2018.1672892e.csv"
+  },
+  {
+    "revision": "2517c8f438a932f7e9433eb9f447e17f",
+    "url": "/Districts_in_HK/static/media/0600_1000_2018.2517c8f4.csv"
+  },
+  {
+    "revision": "31c2713c094114ed0d1b4ae8b7b186f7",
+    "url": "/Districts_in_HK/static/media/1100_1300_2018.31c2713c.csv"
+  },
+  {
+    "revision": "c2c0506494954313b00396e05ce7c89c",
+    "url": "/Districts_in_HK/static/media/1400_1800_2018.c2c05064.csv"
+  },
+  {
+    "revision": "3c95da87ad879462f467d02ef8d53127",
+    "url": "/Districts_in_HK/static/media/1900_2400_2018.3c95da87.csv"
+  },
+  {
+    "revision": "72dbb37472de3786b9411aa19a5d335b",
+    "url": "/Districts_in_HK/static/media/2018.72dbb374.csv"
+  }
+]);
