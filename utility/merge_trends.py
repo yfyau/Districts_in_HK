3002413017c76b7517beafc7589560ef23dcb9bd
Miaomeow <hslamaa@connect.ust.hk>
2019-05-02 22:52:14 +0800

    move area words to district

--- utility/merge_trends.py
+++ utility/merge_trends.py
@@ -2,21 +2,62 @@ import glob
 import errno
 import json
 
-path = r'C:\Users\Jason\Desktop\Districts_in_HK\web\src\data\google-trends\屯門區'
+data_folder = glob.glob(r"..\src\data\google-trends")
+
+
+
+path = r'..\src\data\google-trends\屯門區'
 files = glob.glob(path+"\*.json")
 new_json = []
+new_strings = ""
 
 for name in files:
     try:
         with open(name,"r",encoding="utf-8") as f:
             json_data = json.load(f)
             for obj in json_data:
-                new_json.append(obj)
+                
+                if obj['query'] not in new_strings:
+                    new_json.append(obj)
+                    new_strings += obj['query']
+
     except IOError as exc:
         if exc.errno != errno.EISDIR:
             raise
 
 new_json = sorted(new_json, key=lambda x: x['value'], reverse=True)
+import glob
+import errno
+import json
+
+data_folder = glob.glob(r"..\src\data\google-trends\*")
+
+for district in data_folder:
+
+    district_name = district.split('\\')[-1]
+
+    print(district_name)
+
+    files = glob.glob(district+r"\*.json")
+    new_json = []
+    new_strings = ""
+
+    for name in files:
+        try:
+            with open(name,"r",encoding="utf-8") as f:
+                json_data = json.load(f)
+                for obj in json_data:
+                    
+                    if obj['query'] not in new_strings:
+                        new_json.append(obj)
+                        new_strings += obj['query']
+        except IOError as exc:
+            if exc.errno != errno.EISDIR:
+                raise
+
+    new_json = sorted(new_json, key=lambda x: x['value'], reverse=True)
+    with open(district+'\\new.json', 'w', encoding='utf-8') as outfile:
+        json.dump(new_json[0:14], outfile)
 
 print(new_json[0:19])
 
